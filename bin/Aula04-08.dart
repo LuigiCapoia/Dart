da1e246be2f72026ddb70c3fff9a25f1703e9ab1
Luigi Capoia <luigicapoia@gmail.com>
2023-08-04 23:36:56 +0000

    primeiro commit

---
+++ bin/Aula04-08.dart
@@ -0,0 +1,23 @@
+import 'package:dart/dart.dart' as dart;
+
+String? nome;// nulo ? e late ocorre um atraso uma espera
+
+void main(List<String> arguments) {
+  //
+  // var nome = 'Luigi capoia';
+  // String outroNome = 'Kern';
+  // dynamic tipoDinamico = 'Polloni';
+  // tipoDinamico = 1;
+  // print(nome);
+  // print(outroNome);
+  // print(tipoDinamico);
+ //
+ //
+// nome =_getNome();
+//
+  return;
+}
+
+// String _getNome(){
+//   return '';
+// }
