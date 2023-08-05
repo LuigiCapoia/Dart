a50897a718c5d16935c99edf77f49d18368bdafa
Luigi Capoia <luigicapoia@gmail.com>
2023-08-05 01:25:06 +0000

    exercicio ainda não resolvido

--- bin/Aula04-08.dart
+++ bin/Aula04-08.dart
@@ -1,9 +1,9 @@
 import 'package:dart/dart.dart' as dart;
 
-String? nome;// nulo ? e late ocorre um atraso uma espera
+//String? nome; // nulo ? e late ocorre um atraso uma espera
 
 void main(List<String> arguments) {
-  //
+  //STRING
   // var nome = 'Luigi capoia';
   // String outroNome = 'Kern';
   // dynamic tipoDinamico = 'Polloni';
@@ -11,10 +11,57 @@ void main(List<String> arguments) {
   // print(nome);
   // print(outroNome);
   // print(tipoDinamico);
- //
- //
-// nome =_getNome();
-//
+  //
+  //
+  // nome =_getNome();
+  //
+  // LISTA / STRING
+  // const listaDeCompras = ['arroz', 'feijao']; // imutavel
+  // var listaDeCompras = ["macarrão"];
+  // Set<String> listaComSet = {"oi dlc"}; //lista com set ele não duplica itens
+  // List<String> outraLista = ["uiii"]; //lista que pode repitir elementos contando que seja do mesmo tipo
+  // final listaDeItens = ['monitor', 'teclado'];
+  // listaDeItens.add('computador');
+  // listaDeCompras.clear(); // limpa a lista tem varios outros que podem fazer varis coisas
+  // listaDeItens.removeAt(1); // remove com base no index
+  // print(outraLista);
+  // print(listaComSet);
+  // print(listaDeCompras);
+  // print(listaDeItens);
+  //
+  //MAP
+  // Map<String, int> salas = {'Ligi': 13,"karen":14};//pode ser criado de outa forma
+  // var sala ={'Ligi': 13,"karen":14};
+  // var sal = Map<String ,int>();
+  // sal = {'Ligi': 13,"karen":14};
+  // print(sal);
+  // print(salas);
+  // print(sala);
+  //
+  //SPREAD
+  // var lista = ['arroz', 'fejão','carne'];
+  // var novaLista =['macarrao',lista];
+  // var nnovaLista =['macarrao',...lista]; // com spread
+  // print(nnovaLista);
+  // print(novaLista);
+  //
+  //FUNÇÕES
+  //minhaFuncao (){};
+  //
+  //OPERADORES
+  // +
+  // -
+  // /
+  // *
+  // ~/ pega o grupo
+  // % o que sobrou
+  // var numero =1;
+  // numero ++;// retorna o numero depois faz a dição
+  // ++numero; // faz a adição depois retorna o numero
+  // print(numero);
+  // is server para verificar tipo e is! negação
+  // as para atribuir tipo
+  //
   return;
 }
 
---
+++ bin/exercicio01.dart
@@ -0,0 +1,8 @@
+import 'dart:ffi';
+
+void main(List<String> arguments){
+  var alunos = Map<String , Map<String , List<int>>>();
+
+
+  return;
+}
